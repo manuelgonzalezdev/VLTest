89a0cd7d1a5ae558291bce152844310311f21e2c
Manuel J. Gonzalez <manuelgonzalez.dev@gmail.com>
2018-09-25 00:55:30 +0200

    + Basic materials + Enemy movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VLTest.Enemy
+{
+    public class Movement : MonoBehaviour
+    {
+        Rotator rotator;
+
+        private void Awake()
+        {
+            rotator = GetComponent<Rotator>();
+        }
+
+        private void Start()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            Vector3 pivot = transform.position + (transform.forward * 0.5f);
+            pivot.y -= 0.5f;
+            rotator.Rotate(pivot, 90, 100, Move);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace VLTest.Enemy
+{
+    public class Rotator : MonoBehaviour
+    {
+        private bool rotating;
+
+        private Quaternion lastRotation;
+        private Vector3 pivot;
+        private float remainingDegrees;
+        private float speed;
+        private Action callback;
+
+        public void Rotate(Vector3 pivot, float angle = 90f, float speed = 100f, Action callback = null)
+        {
+            lastRotation = transform.rotation;
+            this.pivot = pivot;
+            remainingDegrees = angle;
+            this.speed = speed;
+            this.callback = callback;
+
+            rotating = true;
+        }
+
+        void Update()
+        {
+            if (rotating)
+            {
+                if (remainingDegrees > 0f)
+                {
+                    float angle = speed * Time.deltaTime;
+                    if (remainingDegrees - angle < 0)
+                    {
+                        angle = remainingDegrees;
+                    }
+                    transform.RotateAround(pivot, transform.right, angle);
+                    remainingDegrees -= angle;
+                }
+                else
+                {
+                    transform.rotation = lastRotation;
+                    rotating = false;
+                    if (callback != null)
+                    {
+                        callback.Invoke();
+                    }
+                }
+            }
+        }
+    }
+}
